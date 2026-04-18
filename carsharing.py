bc7ab8c1a8d5b3f7d66663fa67e7a61e8a5f22e6
RANJITH KUMAR <theranjith@gmail.com>
2026-04-18 15:37:41 +0530

    get cars

--- carsharing.py
+++ carsharing.py
@@ -15,14 +15,14 @@ async def welcome(name):
 def get_cars(size:str|None=None, doors:int|None=None)->list:
     result = db
     if size:
-        result = [car for car in result if car['size'] == size]
+        result = [car for car in result if car.size == size]
     if doors:
-        result = [car for car in result if car['doors'] >= doors]
+        result = [car for car in result if car.doors >= doors]
     return result
 
 @app.post("/api/cars/{id}")
-def get_car(id:int)->dict:
-    result = [car for car in db if car['id'] == id]
+def get_car(id:int):
+    result = [car for car in db if car.id == id]
     if result:
         return result[0]
     else:
